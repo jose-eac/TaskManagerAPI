be34f6b9534c8d83a41c3ccafbe42c76c2e911d5
JoseArias <joseea@teamventi.com>
2024-10-04 17:31:41 -0600

    Add project files.

---
+++ TaskManagerAPI/Models/TaskDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagerAPI.Models
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext> options) 
+            : base(options) 
+        { 
+        }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
+    }
+}
---
+++ TaskManagerAPI/Models/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerAPI.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "The Name field is required.")]
+        [StringLength(100, ErrorMessage = "The Name field cannot exceed 100 characters.")]
+        public string? Name { get; set; }
+        [Required]
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public int Priority { get; set; }
+    }
+}
